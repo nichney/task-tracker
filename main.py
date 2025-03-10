5f4e228fb62bb16740ed375b01b3e40c6db85c95
Osmolovsky K. <kirillosm09@gmail.com>
2025-03-10 18:52:30 +0300

    Add and Update parsing

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python
 # A simple CLI Task Tracker
 import argparse
+import sys
+
+def on_add(args):
+    pass
+
+def on_update(args):
+    pass
+
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
             prog='task-tracker',
             description='A simple CLI Task Tracker')
-    commands = ('add', 'update', 'delete', 'mark-in-progress', 'mark-done', 'list')
-    for command in commands:
-        parser.add_argument(command) 
+    subparsers = parser.add_subparsers(required=True)
+
+    # for add
+    add_parser = subparsers.add_parser('add', help='Add a new task')
+    add_parser.add_argument('text', nargs='+', help='Task text')
+    add_parser.set_defaults(func=on_add)
+
+    # for update
+    update_parser = subparsers.add_parser('update', help='Update a task')
+    update_parser.add_argument('id', type=int, help='Task ID')
+    update_parser.add_argument('text', nargs='+', help='New task text')
+    update_parser.set_defaults(func=on_update)
+
+    args=parser.parse_args()
+    args.func(args)
